a49c59f90f502cd6c027bfa9be8cee11a3392a0a
blurryfacebeat <blurryfacebeat@gmail.com>
2023-04-27 21:57:16 +0700

    feat: Add comment form

---
+++ src/components/CommentForm/CommentForm.tsx
@@ -0,0 +1,26 @@
+import React, { ChangeEvent, FormEvent, useState } from 'react';
+
+import styles from './CommentForm.module.scss';
+
+const CommentForm = () => {
+  const [value, setValue] = useState('');
+
+  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(event.target.value);
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <textarea className={styles.input} value={value} onChange={handleChange} />
+      <button className={styles.button} type="submit">
+        Комментировать
+      </button>
+    </form>
+  );
+};
+
+export default CommentForm;
